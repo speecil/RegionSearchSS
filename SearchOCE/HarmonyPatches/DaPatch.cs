0598da637af134ffeb7ac72928787319aa2c2e91
Riley <90689870+speecil@users.noreply.github.com>
2023-08-25 23:42:21 +1000

    Add project files.

---
+++ SearchOCE/HarmonyPatches/DaPatch.cs
@@ -0,0 +1,66 @@
+﻿using HarmonyLib;
+using System;
+using System.Reflection;
+
+namespace SearchOCE.HarmonyPatches
+{
+    [HarmonyPatch]
+    internal class DaPatch
+    {
+        [HarmonyTargetMethod]
+        public static MethodBase TargetMethod()
+        {
+            Type leaderboardServiceType = typeof(ScoreSaber.Plugin).Assembly.GetType("ScoreSaber.Core.Services.LeaderboardService");
+            return leaderboardServiceType.GetMethod("GetLeaderboardUrl", BindingFlags.Instance | BindingFlags.NonPublic, null, new Type[] { typeof(IDifficultyBeatmap), typeof(PlatformLeaderboardsModel.ScoresScope), typeof(int), typeof(bool) }, null);
+        }
+
+
+        [HarmonyPrefix]
+        public static bool Prefix(ref string __result, IDifficultyBeatmap difficultyBeatmap, PlatformLeaderboardsModel.ScoresScope scope, int page, bool filterAroundCountry)
+        {
+            Plugin.Log.Info("FORTNITE PREFIX");
+            string difficulty = BeatmapDifficultyMethods.DefaultRating(difficultyBeatmap.difficulty).ToString();
+            if (filterAroundCountry)
+            {
+                __result = $"leaderboard/by-hash/{difficultyBeatmap.level.levelID.Split('_')[2]}/scores?difficulty={difficulty}&countries=AU,NZ&page={page}";
+                return false;
+            }
+            return true;
+        }
+    }
+
+    //[HarmonyPatch]
+    //internal class DaPatch2
+    //{
+    //    [HarmonyTargetMethod]
+    //    public static MethodBase TargetMethod()
+    //    {
+    //        Type leaderboardServiceType = typeof(ScoreSaber.Plugin).Assembly.GetType("ScoreSaber.Core.Services.LeaderboardService");
+    //        return leaderboardServiceType.GetMethod("GetLeaderboardData", BindingFlags.Instance | BindingFlags.Public, null, new Type[] { typeof(IDifficultyBeatmap), typeof(PlatformLeaderboardsModel.ScoresScope), typeof(PlayerSpecificSettings), typeof(bool) }, null);
+    //    }
+
+
+    //    [HarmonyPrefix]
+    //    public static bool Prefix(object __instance, IDifficultyBeatmap difficultyBeatmap, PlatformLeaderboardsModel.ScoresScope scope, PlayerSpecificSettings playerSpecificSettings, bool filterAroundCountry)
+    //    {
+    //        if (!filterAroundCountry) return false;
+
+    //        Type instanceType = __instance.GetType();
+    //        FieldInfo leaderboardRawDataField = instanceType.GetField("leaderboardRawData", BindingFlags.NonPublic | BindingFlags.Instance);
+
+    //        if (leaderboardRawDataField != null)
+    //        {
+    //            string leaderboardRawData = (string)leaderboardRawDataField.GetValue(__instance);
+
+    //            // magic
+
+    //            leaderboardRawDataField.SetValue(__instance, leaderboardRawData);
+    //        }
+
+    //        return true;
+    //    }
+    //}
+
+}
+
+
---
+++ SearchOCE/Plugin.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using IPA;
+using System.Reflection;
+using UnityEngine;
+using IPALogger = IPA.Logging.Logger;
+namespace SearchOCE
+{
+    [Plugin(RuntimeOptions.SingleStartInit)]
+    public class Plugin
+    {
+        internal static Plugin Instance { get; private set; }
+        internal static IPALogger Log { get; private set; }
+
+        public static Harmony harmony;
+
+        [Init]
+        public void Init(IPALogger logger)
+        {
+            Instance = this;
+            Log = logger;
+            harmony = new Harmony("Speecil.BeatSaber.SearchOCE");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
